05fb9e502a201d10766e8811ef6f9b2e6e3c603b
lamyaHM <109969930+lamyaHM@users.noreply.github.com>
2022-08-03 13:45:04 +0200

    calcule
    calculatrice basique calcule une somme,
    une soustraction, une multiplication, une division, entre deux num .
    Le programme  s’arrêter lorsque la lettre « q » est inserer comme opération

---
+++ calc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main() {
+    float a;
+    float b; 
+    float c;
+    char opp ; 
+    printf("entrer une valeur ");
+    scanf("%f", &a);
+    printf ("entrer une valeur ");
+    scanf ("%f",&b);
+    printf("entrer l'operastion souhaiter");
+    scanf("%s",&opp);
+   switch (opp)
+   {
+      case '+':
+      printf("%.2f",c = a + b);
+      break;
+      case '-'  :
+      printf("%.2f", c = a - b);
+      break;
+      case '*' :
+      printf("%.2f", c = a * b);
+      break;
+      case   '/' :
+      printf("%.2f", c = a / b);
+      break;
+      case   'q' :
+      exit(0);
+      break;
+   }   
+   }
+    // return 0;
